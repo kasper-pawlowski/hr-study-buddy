9e4bfb450c5e954a4c800ba0c079aad977d58edf
kasper-pawlowski <kapi28000@gmail.com>
2021-11-21 22:17:34 +0100

    styling

---
+++ src/components/oraganisms/UsersList/UsersList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { users } from 'data/users';
+import UsersListItem from 'components/molecules/UsersListItem/UserListItem';
+import { Wrapper } from './UsersList.styles';
+
+const UsersList = () => (
+    <Wrapper>
+        <ul>
+            {users.map((userData) => (
+                <UsersListItem userData={userData} />
+            ))}
+        </ul>
+    </Wrapper>
+);
+
+export default UsersList;
