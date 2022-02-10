99e1bdc474114b382c553f0b58a59d95344cd292
kasper-pawlowski <kapi28000@gmail.com>
2022-02-10 22:21:20 +0100

    new stuff

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navigation from 'components/oraganisms/Navigation/Navigation';
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
+import SearchBar from 'components/oraganisms/SearchBar/SearchBar';
+import NewsSection from '../NewsSection/NewsSection';
 
 const MainTemplate = ({ children }) => {
     return (
         <Wrapper>
             <Navigation />
+            <SearchBar />
+            <NewsSection />
             {children}
         </Wrapper>
     );
