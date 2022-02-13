4c1ff5edbae8de6ae535cc87b2337f2e3fefb957
kasper-pawlowski <kapi28000@gmail.com>
2022-02-14 00:08:06 +0100

    added downshift to SearchBar

--- src/components/organisms/SearchBar/SearchBar.js
+++ src/components/organisms/SearchBar/SearchBar.js
@@ -1,42 +1,59 @@
 import { Input } from 'components/atoms/Input/Input';
 import React, { useState, useEffect } from 'react';
 import debounce from 'lodash.debounce';
-import { SearchBarWrapper, SearchResults, SearchWrapper, StatusInfo } from 'components/organisms/SearchBar/SearchBar.styles';
+import {
+    SearchBarWrapper,
+    SearchResults,
+    SearchWrapper,
+    StatusInfo,
+} from 'components/organisms/SearchBar/SearchBar.styles';
 import { useStudents } from 'hooks/useStudents';
+import { useCombobox } from 'downshift';
 
 export const SearchBar = () => {
-  const [searchPhrase, setSearchPhrase] = useState('');
-  const [matchingStudents, setMatchingStudents] = useState('');
-  const { findStudents } = useStudents();
+    const [matchingStudents, setMatchingStudents] = useState([]);
+    const { findStudents } = useStudents();
 
-  const getMatchingStudents = debounce(async (e) => {
-    const { students } = await findStudents(searchPhrase);
-    setMatchingStudents(students);
-  }, 500);
+    const getMatchingStudents = debounce(async ({ inputValue }) => {
+        const { students } = await findStudents(inputValue);
+        setMatchingStudents(students);
+    }, 500);
 
-  useEffect(() => {
-    if (!searchPhrase) return;
-    getMatchingStudents(searchPhrase);
-  }, [searchPhrase, getMatchingStudents]);
+    const {
+        isOpen,
+        getToggleButtonProps,
+        getLabelProps,
+        getMenuProps,
+        getInputProps,
+        getComboboxProps,
+        highlightedIndex,
+        getItemProps,
+    } = useCombobox({
+        items: matchingStudents,
+        onInputValueChange: getMatchingStudents,
+    });
 
-  return (
-    <SearchBarWrapper>
-      <StatusInfo>
-        <p>Logged as:</p>
-        <p>
-          <strong>Teacher</strong>
-        </p>
-      </StatusInfo>
-      <SearchWrapper>
-        <Input onChange={(e) => setSearchPhrase(e.target.value)} value={searchPhrase} name="Search" id="Search" />
-        {searchPhrase && matchingStudents.length ? (
-          <SearchResults>
-            {matchingStudents.map((student) => (
-              <li key={student.id}>{student.name}</li>
-            ))}
-          </SearchResults>
-        ) : null}
-      </SearchWrapper>
-    </SearchBarWrapper>
-  );
+    return (
+        <SearchBarWrapper>
+            <StatusInfo>
+                <p>Logged as:</p>
+                <p>
+                    <strong>Teacher</strong>
+                </p>
+            </StatusInfo>
+            <SearchWrapper {...getComboboxProps()}>
+                <Input {...getInputProps()} name="Search" id="Search" />
+                <SearchResults {...getMenuProps()}>
+                    {isOpen &&
+                        matchingStudents.map((item, index) => (
+                            <li {...getItemProps({ item, index })} key={item.id}>
+                                {item.name}
+                            </li>
+                        ))}
+                </SearchResults>
+            </SearchWrapper>
+        </SearchBarWrapper>
+    );
 };
+
+//16
