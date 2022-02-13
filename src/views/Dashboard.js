7dba5633c9f7a07904ba1220929066a6d88d8960
kasper-pawlowski <kapi28000@gmail.com>
2022-02-13 23:35:23 +0100

    test

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -7,28 +7,29 @@ import { GroupWrapper, TitleWrapper, Wrapper } from 'views/Dashboard.styles';
 import { Title } from 'components/atoms/Title/Title';
 
 const Dashboard = () => {
-  const { groups } = useStudents();
-  const { id } = useParams();
+    const { groups } = useStudents();
+    const { id } = useParams();
 
-  if (!id && groups.length > 0) return <Redirect to={`/group/${groups[0]}`} />;
+    if (!id && groups.length > 0) return <Redirect to={`/group/${groups[0]}`} />;
 
-  return (
-    <Wrapper>
-      <TitleWrapper>
-        <Title as="h2">Group {id}</Title>
-        <nav>
-          {groups.map((group) => (
-            <Link key={group} to={`/group/${group}`}>
-              {group}{' '}
-            </Link>
-          ))}
-        </nav>
-      </TitleWrapper>
-      <GroupWrapper>
-        <StudentsList />
-      </GroupWrapper>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <TitleWrapper>
+                <Title as="h2">Group {id}</Title>
+                <nav>
+                    {groups.map((group) => (
+                        <Link key={group} to={`/group/${group}`}>
+                            {group}{' '}
+                        </Link>
+                    ))}
+                </nav>
+            </TitleWrapper>
+            <GroupWrapper>
+                <StudentsList />
+            </GroupWrapper>
+            <p>downshift</p>
+        </Wrapper>
+    );
 };
 
 export default Dashboard;
